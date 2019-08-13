03417dfc28465b8017e5b6d99be150523a168616
Adam Niedzwiecki <adamn8142@gmail.com>
2019-08-12 19:31:25 -0600

    Pass props to card

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -7,6 +7,7 @@
 <script>
   export default {
     name: 'Card',
+    props: ['images']
   }
 </script>
 
--- src/components/CardContainer.vue
+++ src/components/CardContainer.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <Card />
+    <Card v-bind:images="images"/>
   </div>
 </template>
 
